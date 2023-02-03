c19d3dda50526848821df9bd392e121023608aa4
panda <panda@vip.com>
2023-02-03 12:47:19 +0800

    init vue3+vite+ts

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -30,24 +30,22 @@ export default defineComponent({
         />
         <ContainerView>
           <Tabs ellipsis={false}>
-            {window.dataJson.pageData.map((item: any, index: number) => (
+            {window.dataJson.pageData.map((item:any, index:number) => (
               <Tab title={item.tabName}>
                 <div class="home-banner">
                   <Swipe autoplay={5000} indicator-color="white">
-                    {item.data.banner.map(
-                      (bannerItem: any, bannerIndex: number) => (
-                        <SwipeItem key={bannerIndex}>
-                          <Image src={bannerItem} />
-                        </SwipeItem>
-                      )
-                    )}
+                    {item.data.banner.map((bannerItem:any, bannerIndex:number) => (
+                      <SwipeItem key={bannerIndex}>
+                        <Image src={bannerItem} />
+                      </SwipeItem>
+                    ))}
                   </Swipe>
                 </div>
-                {item.data.list.map((dataItem: any) => (
+                {item.data.list.map((dataItem:any) => (
                   <div class="home-tabs">
                     <h1 class="home-tabs-title">{dataItem.title}</h1>
                     <div class="home-tab-swipe">
-                      <Swipe loop={false}  indicator-color="white">
+                      <Swipe loop={false} width={90} indicator-color="white">
                         {dataItem.list.map(
                           (listItem: any, listIndex: number) => (
                             <SwipeItem key={listIndex}>
