86f63e861a13e1782331d290aa72d17cffd93387
panda <panda@vip.com>
2023-02-03 12:29:20 +0800

    init vue3+vite+ts

--- public/config.js
+++ public/config.js
@@ -1,3 +1,12 @@
+/**
+ * 页面数据
+ * logo: logo图标(尺寸:30x30)
+ * pageData.tabName: 导航名
+ * pageData.footerUrl: 底部推荐图(尺寸:375x200)
+ * pageData.data.banner: 轮播图(尺寸:375x150)
+ * pageData.data.list: 页面列表数据(icon:图标尺寸60x60)
+ */
+
 window.dataJson = {
   // logo图标(尺寸:30x30)
   logo: "https://android-artworks.25pp.com/fs08/2023/01/10/7/106_d8197aa728135b1b9bab6abb1e75d437_con_130x130.png",
@@ -15,7 +24,7 @@ window.dataJson = {
           "https://d540vms5r2s2d.cloudfront.net/mad/uploads/mad_blog_5ba0f03981cec1537273913.png",
           "https://www.gurutechnolabs.com/wp-content/uploads/2019/10/turn-website-into-mobile-app.jpg",
         ],
-        //小图标尺寸:375x150
+        //小图标尺寸:60x60
         list: [
           {
             title: "装机必备",
@@ -183,7 +192,7 @@ window.dataJson = {
           "https://android-imgs.25pp.com/fs08/2023/02/01/9/2_38baedfc06e8fb438a69e2fa8e2e51e8.jpg?x-oss-process=image/watermark,image_d2RqX2ljb24ucG5nP3gtb3NzLXByb2Nlc3M9aW1hZ2UvcmVzaXplLFBfMzA=,x_30,y_20,t_75",
           "https://android-imgs.25pp.com/fs08/2023/02/01/3/1_a6862f6df88eb8c00d92329d73359b63.jpg?x-oss-process=image/watermark,image_d2RqX2ljb24ucG5nP3gtb3NzLXByb2Nlc3M9aW1hZ2UvcmVzaXplLFBfMzA=,x_30,y_20,t_75",
         ],
-        //小图标尺寸:375x150
+        //小图标尺寸:60x60
         list: [
           {
             title: "装机必备",
@@ -354,7 +363,7 @@ window.dataJson = {
           "https://android-imgs.25pp.com/fs08/2023/02/01/9/2_38baedfc06e8fb438a69e2fa8e2e51e8.jpg?x-oss-process=image/watermark,image_d2RqX2ljb24ucG5nP3gtb3NzLXByb2Nlc3M9aW1hZ2UvcmVzaXplLFBfMzA=,x_30,y_20,t_75",
           "https://android-imgs.25pp.com/fs08/2023/02/01/3/1_a6862f6df88eb8c00d92329d73359b63.jpg?x-oss-process=image/watermark,image_d2RqX2ljb24ucG5nP3gtb3NzLXByb2Nlc3M9aW1hZ2UvcmVzaXplLFBfMzA=,x_30,y_20,t_75",
         ],
-        //小图标尺寸:375x150
+        //小图标尺寸:60x60
         list: [
           {
             title: "装机必备",
@@ -525,7 +534,7 @@ window.dataJson = {
           "https://android-imgs.25pp.com/fs08/2023/02/01/9/2_38baedfc06e8fb438a69e2fa8e2e51e8.jpg?x-oss-process=image/watermark,image_d2RqX2ljb24ucG5nP3gtb3NzLXByb2Nlc3M9aW1hZ2UvcmVzaXplLFBfMzA=,x_30,y_20,t_75",
           "https://android-imgs.25pp.com/fs08/2023/02/01/3/1_a6862f6df88eb8c00d92329d73359b63.jpg?x-oss-process=image/watermark,image_d2RqX2ljb24ucG5nP3gtb3NzLXByb2Nlc3M9aW1hZ2UvcmVzaXplLFBfMzA=,x_30,y_20,t_75",
         ],
-        //小图标尺寸:375x150
+        //小图标尺寸:60x60
         list: [
           {
             title: "装机必备",
